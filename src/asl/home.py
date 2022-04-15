bd35d1db2dd2b8bd3abd6d1319990eefead5ff40
Aditi Patil <aap2205@columbia.edu>
2022-04-15 15:31:47 -0400

    add home page nav bar and routes

--- src/asl/home.py
+++ src/asl/home.py
@@ -5,4 +5,25 @@ bp = Blueprint('home', __name__)
 
 @bp.route('/', methods=["GET"])
 def index():
-    return "Hello, World!"
+    return render_template('home.html')
+
+@bp.route('/greetings', methods=["GET"])
+def greeting():
+    return render_template('greetings.html')
+
+@bp.route('/manners', methods=["GET"])
+def manner():
+    return render_template('manners.html')
+
+@bp.route('/learning', methods=["GET"])
+def learning():
+    return render_template('learning.html')
+
+@bp.route('/farewell', methods=["GET"])
+def farewell():
+    return render_template('farewell.html')
+
+@bp.route('/quiz', methods=["GET"])
+def quiz():
+    return render_template('quiz.html')
+
