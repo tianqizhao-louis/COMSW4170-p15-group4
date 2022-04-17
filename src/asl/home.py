ddde0cfe6e24355cab1b877e7dc67b22885fdb43
tianqizhao-louis <louiszhao0517@gmail.com>
2022-04-17 17:12:26 -0400

    reformatting

--- src/asl/home.py
+++ src/asl/home.py
@@ -2,14 +2,12 @@ from flask import (Blueprint, Flask, flash, g, redirect, render_template, reques
 import os
 
 bp = Blueprint('home', __name__)
-app = Flask(__name__)
 
-with open('./asl/static/quiz.json') as json_file:
-    quiz_data = json.load(json_file)
 
 @bp.route('/', methods=["GET"])
 def index():
     return render_template('home.html')
+
 
 @bp.route('/greetings/<videoId>', methods=["GET"])
 def greeting(videoId):
@@ -21,6 +19,7 @@ def greeting(videoId):
                 lesson[item['videoId']] = item
     return render_template('greetings.html', lesson=lesson)
 
+
 @bp.route('/manners/<videoId>', methods=["GET"])
 def manner(videoId):
     with open('./asl/static/learning.json') as json_file:
@@ -30,6 +29,7 @@ def manner(videoId):
             if item['videoId'] == int(videoId):
                 lesson[item['videoId']] = item
     return render_template('manners.html', lesson=lesson)
+
 
 @bp.route('/learning/<videoId>', methods=["GET"])
 def learning(videoId):
@@ -41,6 +41,7 @@ def learning(videoId):
                 lesson[item['videoId']] = item
     return render_template('learning.html', lesson=lesson)
 
+
 @bp.route('/farewell/<videoId>', methods=["GET"])
 def farewell(videoId):
     with open('./asl/static/learning.json') as json_file:
@@ -51,23 +52,26 @@ def farewell(videoId):
                 lesson[item['videoId']] = item
     return render_template('farewell.html', lesson=lesson)
 
+
 # quiz starting page
 @bp.route('/quiz', methods=["GET"])
 def quiz():
     return render_template('quiz.html')
 
+
 # quiz question page
 @bp.route('/question/<id>')
 def question(id=None):
+    with open('./asl/static/quiz.json') as json_file:
+        quiz_data = json.load(json_file)
     # get to correct quiz question
     return render_template('question.html', quiz_data=quiz_data, id=id)
+
 
 @bp.route('/end')
 def end():
     return render_template('end.html')
 
-"""
-@bp.route('/greetings/<videoid>', methods=["GET"])
-def greeting_lesson(videoid):
-    return render_template('greetings.html', id=id)
-"""
+# @bp.route('/greetings/<videoid>', methods=["GET"])
+# def greeting_lesson(videoid):
+#     return render_template('greetings.html', id=id)
