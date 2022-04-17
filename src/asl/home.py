7c30d0d0a475a5c87b7577ef78e786e881ccdcef
Aditi Patil <aap2205@columbia.edu>
2022-04-17 15:28:21 -0400

    Create greetings and Manners sections

--- src/asl/home.py
+++ src/asl/home.py
@@ -8,26 +8,45 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
-@bp.route('/greetings', methods=["GET"])
-def greeting():
+@bp.route('/greetings/<videoId>', methods=["GET"])
+def greeting(videoId):
     with open('./asl/static/learning.json') as json_file:
         data = json.load(json_file)
         lesson = {}
         for item in data:
-            lesson[item['videoId']] = item
+            if item['videoId'] == int(videoId):
+                lesson[item['videoId']] = item
     return render_template('greetings.html', lesson=lesson)
 
-@bp.route('/manners', methods=["GET"])
-def manner():
-    return render_template('manners.html')
+@bp.route('/manners/<videoId>', methods=["GET"])
+def manner(videoId):
+    with open('./asl/static/learning.json') as json_file:
+        data = json.load(json_file)
+        lesson = {}
+        for item in data:
+            if item['videoId'] == int(videoId):
+                lesson[item['videoId']] = item
+    return render_template('manners.html', lesson=lesson)
 
-@bp.route('/learning', methods=["GET"])
-def learning():
-    return render_template('learning.html')
+@bp.route('/learning/<videoId>', methods=["GET"])
+def learning(videoId):
+    with open('./asl/static/learning.json') as json_file:
+        data = json.load(json_file)
+        lesson = {}
+        for item in data:
+            if item['videoId'] == int(videoId):
+                lesson[item['videoId']] = item
+    return render_template('learning.html', lesson=lesson)
 
-@bp.route('/farewell', methods=["GET"])
-def farewell():
-    return render_template('farewell.html')
+@bp.route('/farewell/<videoId>', methods=["GET"])
+def farewell(videoId):
+    with open('./asl/static/learning.json') as json_file:
+        data = json.load(json_file)
+        lesson = {}
+        for item in data:
+            if item['videoId'] == int(videoId):
+                lesson[item['videoId']] = item
+    return render_template('farewell.html', lesson=lesson)
 
 @bp.route('/quiz', methods=["GET"])
 def quiz():
